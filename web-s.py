d0a1f3cb63f66ef9c5acb2880c958a5765dcd684
prafful360 <praffulranjan54@gmail.com>
2020-02-26 01:50:04 +0530

    Price and Name Fetching

--- web-s.py
+++ web-s.py
@@ -1,24 +1,39 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 
 driver = webdriver.Chrome("C:\\Users\\kpraf\\Downloads\\chromedriver_win32\\chromedriver.exe")
 
 products=[]
 prices=[]
 #ratings=[]
-driver.get('https://www.flipkart.com/computers/laptops/pr?sid=6bo,b5g&q=laptops&otracker=categorytree')
+driver.get('https://www.mysmartprice.com/mobile/pricelist/samsung-mobile-price-list-in-india.html')
 
 content = driver.page_source
 soup = BeautifulSoup(content, "html.parser")
 
-for a in soup.findAll('a',href=True, attrs={'class':'_31qSD5'}):
-    name=a.find('div', attrs={'class':'_3wU53n'})
-    price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
-    #rating=a.find('div', attrs={'class':'hGSR34'})
-    products.append(name.text)
-    prices.append(price.text)
-    #ratings.append(rating.text)
+list_containers = soup.findAll('div', attrs={"class":"prdct-item__dtls"})
+#print(type(list_containers))
+#print(len(list_containers))
+
+for div in list_containers:
+    name=div.find('a', attrs={'class':'prdct-item__name'})
+    #print(name)
+    price=div.find('span', attrs={'class':'prdct-item__prc-val'})
+    #print(price)
+
+    if name:
+        products.append(name.text)
+    else:
+        products.append(np.nan)
+    if price:
+        prices.append(price.text)
+    else:
+        prices.append(np.nan)
+next_btn = soup.find('a', attrs={'data-pgno':'2'})
+print(next_btn)
+
 
 df = pd.DataFrame({'Product Name':products,'Price':prices,}) 
 df.to_csv('products.csv', index=False, encoding='utf-8')
